c016852a0699b11eed6587a7c27012fdf3992dc3
liyuxuan <44829092+xmliszt@users.noreply.github.com>
2021-06-22 15:30:01 +0800

    Revert "Revert "feat: set up basic scriptable object variables to use""
    This reverts commit 69b502aeabf65308402e7260be262b24c5c31201.

---
+++ CB2.0/Assets/Scripts/ScriptableObjects/FloatVariable.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+[
+    CreateAssetMenu(
+        fileName = "FloatVar",
+        menuName = "ScriptableObjects/FloatVar",
+        order = 2)
+]
+public class FloatVariable : ScriptableObject
+{
+    private float _value = 0.0f;
+
+    public float Value
+    {
+        get
+        {
+            return _value;
+        }
+    }
+
+    public void Increment(float value)
+    {
+        _value += value;
+    }
+
+    public void Decrement(float value)
+    {
+        _value -= value;
+    }
+
+    public void Set(float value)
+    {
+        _value = value;
+    }
+
+    public void Set(FloatVariable value)
+    {
+        _value = value.Value;
+    }
+}
