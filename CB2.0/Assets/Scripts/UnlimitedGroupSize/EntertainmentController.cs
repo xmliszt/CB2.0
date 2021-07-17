cbe36084a43b5791b9421c6fb66fc6cc0291f464
weepz <drantinire@gmail.com>
2021-07-18 01:52:55 +0800

    feat: npc count on entertainment items

--- CB2.0/Assets/Scripts/UnlimitedGroupSize/EntertainmentController.cs
+++ CB2.0/Assets/Scripts/UnlimitedGroupSize/EntertainmentController.cs
@@ -4,16 +4,53 @@ using UnityEngine;
 
 public class EntertainmentController : MonoBehaviour
 {
+    [Header("Player")]
     public GameObject fromPlayer;
 
+    [Header("UI")]   
     public Color color;
+    public GameObject[] npcList;
 
     private SpriteRenderer entertainmentSprite;
+
+    private int attractLevel;
+
+    private GameObject npcUI;
+
+    
+    private int generateAttractLevel() {
+        float rand = Random.value;
+        
+        // Probability: 1 (0.35), 2 (0.30), 3 (0.20), 4 (0.10), 5 (0.05)
+        if (rand <= .35f) {
+            return 1;
+        }
+        else if (rand <= .65f) {
+            return 2;
+        }
+        else if (rand <= .85f) {
+            return 3;
+        }
+        else if (rand <= .95f) {
+            return 4;
+        }
+        return 5;
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
         entertainmentSprite = GetComponent<SpriteRenderer>();
+
+        attractLevel = generateAttractLevel();
+
+        for (int i = 0; i < attractLevel; i++) {
+            npcList[i].SetActive(true);
+        }
+
     }
+
+
 
     // Update is called once per frame
     void Update()
