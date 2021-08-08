a0c9731dcfd697fab88306e0c1d9d5c3abb8290f
weepz <drantinire@gmail.com>
2021-08-08 15:47:00 +0800

    feat: dynamic zone colouring for players

---
+++ CB2.0/Assets/Scripts/UnlimitedGroupSize/ZoneColour.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZoneColour : MonoBehaviour {
+    
+    public int playerID;
+    public Players players;
+
+    private Color32 playerColor;
+
+    private RawImage rawImage;
+
+    void Start()
+    {
+        rawImage = GetComponent<RawImage>();
+        playerColor = players.GetPlayers()[playerID].playerStats.playerAccent;
+        rawImage.color = new Color32(playerColor.r, playerColor.g, playerColor.b, 50);
+    }
+}
